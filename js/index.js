651289080125c7fa4712f18fac944d50b021e6c9
Ethan Sutherland <ESutherland95@gmail.com>
2020-12-05 21:36:54 -0600

    started table for general commands

---
+++ js/index.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    var url_string = window.location.href;
+    var url = new URL(url_string);
+
+    var bot_name = url.searchParams.get("bot_name")
+    var prefix = url.searchParams.get("prefix");
+
+    $("#head").html("<h1>Commands for " + bot_name + "</h1>")
+
+    console.log(prefix)
+
+    $("#bot-cmd").text(prefix + "bot")
+    $("#help-cmd").text(prefix + "help")
+    $("#whois-cmd").text(prefix + "whois [@user] - ([]=optional)")
+    $("#purge-cmd").text(prefix + "purge {amount} [@user] - ({}=required, []=optional)")
+    $("#module-cmd").text(prefix + "module {name} {enable/disable} - ({}=required)")
+    $("#modules-cmd").text(prefix + "modules")
+});
