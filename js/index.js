3b7b7e81b6021366dc3a3c1eaa43032eabf96598
Ethan Sutherland <ESutherland95@gmail.com>
2020-12-06 20:54:23 -0600

    updated how prefix is displayed to clean up js file

--- js/index.js
+++ js/index.js
@@ -35,22 +35,5 @@ $(document).ready(function(){
 
 
     $("#head").html("<h1>Commands for " + bot_name + "</h1>")
-    $("#bot-cmd").text(prefix + "bot")
-    $("#help-cmd").text(prefix + "help")
-    $("#whois-cmd").text(prefix + "whois [@user] - ([]=optional)")
-    $("#purge-cmd").text(prefix + "purge {amount} [@user] - ({}=required, []=optional)")
-    $("#module-cmd").text(prefix + "module {name} {enable/disable} - ({}=required)")
-    $("#modules-cmd").text(prefix + "modules")
-    $("#lookup-cmd").text(prefix + "lookup {user ID} - ({}=required)")
-    $("#stop-cmd").text(prefix + "stop")
-    $("#color-cmd").text(prefix + "color/colour {hex} - ({}=required)")
-    $("#level-cmd").text(prefix + "level [@user] - ([]=optional)")
-    $("#leveltop-cmd").text(prefix + "leveltop")
-    $("#addexp-cmd").text(prefix + "addexp {amount} [@user] - ({}=required, []=optional)")
-    $("#whitelist-cmd").text(prefix + "whitelist {MC username} - ({}=required)")
-    $("#commands-cmd").text(prefix + "commands")
-    $("#command-cmd").text(prefix + "command {permission flag} {command} {response} - ({}=required)")
-    $("#update-cmd").text(prefix + "update {command} {response} - ({}=required)")
-    $("#delete-cmd").text(prefix + "delete {command} - ({}=required)")
-    $("#villager-cmd").text(prefix + "villager {name} - ({}=required)")
+    $(".prefix").text(prefix)
 });
